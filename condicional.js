623ca0887ea16c72987c06668474a486869d3181
ERICK BARBOSA DA SILVA <00000830257@unicap.br>
2022-03-21 20:39:31 -0300

    default

---
+++ condicional.js
@@ -0,0 +1,11 @@
+let podeDirigir;
+
+if (idade < 18) {
+    podeDirigir = "não";
+}else{
+    podeDirigir = "sim"
+}
+
+//let podeDirigir = idade < 18 ? "nao" : "sim";
+
+//alert(`Pode dirigir = ${idade < 18 ? "nao" : "sim"}`)
